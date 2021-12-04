897a44fcfd6020473e41bdfa4707772d18a757cf
Andrew Bates <abates@omeganetserv.com>
2021-12-04 15:06:14 -0500

    Migrated to web-dev-server from es-dev-server

--- web-dev-server.config.js
+++ web-dev-server.config.js
@@ -2,12 +2,10 @@ const cors = require('@koa/cors');
 
 module.exports = {
   port: 8000,
-  cors: true,
   watch: true,
   open: false,
   nodeResolve: true,
   appIndex: 'demo/index.html',
   plugins: [],
-  moduleDirs: ['node_modules'],
-  middlewares: [cors()],
+  middleware: [cors()],
 };
